27381a4c1daa4a0f1adaaaa439778c2c4367b8f1
I3ahsoxx <andre.demir@haw-hamburg.de>
2024-01-17 15:46:04 +0100

    Changes by Jimmy

--- KingOfTheCastle/core/src/com/mygdx/kotc/KingOfTheCastle.java
+++ KingOfTheCastle/core/src/com/mygdx/kotc/KingOfTheCastle.java
@@ -3,6 +3,7 @@ package com.mygdx.kotc;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.GL20;
